dc2fbc969ad58d17203897778201d0bd01a2c68b
jembijemb <jembi@protonmail.com>
2019-07-24 22:23:39 +0200

    init: create with action-toolkit and  verbose logs

---
+++ index.js
@@ -0,0 +1,31 @@
+const {Toolkit} = require("actions-toolkit");
+
+const init = tools => {
+  const argsList = tools.arguments._;
+  if (argsList.length < 1)
+    tools.exit.failure("Invalid action usage: context argument required");
+  return {
+    statusContext: argsList[0],
+    statusState: argsList.length < 2 ? "success" : argsList[1]
+  };
+};
+
+Toolkit.run(
+  async tools => {
+    const {statusContext, statusState} = init(tools);
+    const payload = tools.context.payload;
+
+    tools.log("Status payload:", statusPayload);
+    tools.log("Required status context:", statusContext);
+    tools.log("Required status state:", statusState);
+
+    if (payload.context !== statusContext)
+      tools.exit.neutral("Invalid status context", payload.context);
+
+    if (payload.state !== statusState)
+      tools.exit.neutral("Invalid status state", payload.state);
+
+    tools.exit.success("Done:", statusContext, statusState, "confirmed");
+  },
+  {event: "status"}
+);
---
+++ index.test.js
@@ -0,0 +1,24 @@
+const {Toolkit} = require("actions-toolkit");
+
+describe("status-filter", () => {
+  let action, tools;
+
+  // Mock Toolkit.run to define `action` so we can call it
+  Toolkit.run = jest.fn(actionFn => {
+    action = actionFn;
+  });
+  // Load up our entrypoint file
+  require(".");
+
+  beforeEach(() => {
+    // Create a new Toolkit instance
+    tools = new Toolkit();
+    // Mock methods on it!
+    tools.exit.success = jest.fn();
+  });
+
+  it("exits successfully", () => {
+    action(tools);
+    expect(tools.exit.success).toHaveBeenCalled();
+  });
+});
